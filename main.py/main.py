2c136f249c5e6745ff79a0c47295baa1c357cf23
braden <leachbr787@northwested.org>
2024-09-25 13:21:02 -0400

    Create main.py

---
+++ main.py/main.py
@@ -0,0 +1,24 @@
+#Braden Leach
+#Sep 25 2024
+#simple math
+
+#practice 1 
+
+#the actual number
+num1 = 3
+num2 = 4
+#calculations
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+remainder =5 #figure out 
+#
+print(f'{num1} + {num2} = {sum} ')
+print(f'{num1} - {num2} = {difference} ')
+print(f'{num1} * {num2} = {product} ')
+print(f'{num1} / {num2} = {quotient} ')
+
+#practice 2
+
+
