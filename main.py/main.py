8979d7ab230aa585a18d09d0b349bcdb80ec1971
braden <leachbr787@northwested.org>
2024-09-25 13:32:38 -0400

    Update main.py

--- main.py/main.py
+++ main.py/main.py
@@ -4,6 +4,7 @@
 
 #practice 1 
 
+
 #the actual number
 num1 = 3
 num2 = 4
@@ -12,13 +13,28 @@ sum = num1 + num2
 difference = num1 - num2
 product = num1 * num2
 quotient = num1 / num2
-remainder =5 #figure out 
-#
-print(f'{num1} + {num2} = {sum} ')
-print(f'{num1} - {num2} = {difference} ')
-print(f'{num1} * {num2} = {product} ')
-print(f'{num1} / {num2} = {quotient} ')
+remainder =num1 % num2
+#display
+print(f'{num1} plus {num2} = {sum} ')
+print(f'{num1} minus {num2} = {difference} ')
+print(f'{num1} times {num2} = {product} ')
+print(f'{num1} divided by {num2} = {quotient} ')
+print(f'{num1} remainder {num2} = {remainder} ')
+
+
 
 #practice 2
 
+
+#input
+length = float(input('enter the length of a rectangular room (in feet):\n'))
+width =float(input('enter the width of a rectangular room (in feet):\n'))
+#math
+area = length * width
+#display
+print(f'the area of your room is {area} feet squared')
+
+
+
+#
 
